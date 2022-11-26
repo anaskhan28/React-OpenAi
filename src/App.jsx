5393c43297a8dbf27cadc600bd828221ef83cbaa
anaskhan28 <khananasatique@gmail.com>
2022-11-26 11:27:35 +0530

    initial ai generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
+import {Configuration, OpenAIApi} from 'openai'
 import './App.css'
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+   const [result, setResult] = useState('');
+
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () =>{
+     const res = await openai.createImage({
+      prompt: prompt,
+      n:1,
+      size: "1024x1024",
+    });
+    setResult(res.data.data[0].url)
+  }
   return (
-    <div>
-      
+    <div className='app-main'>
+      <h3>Generate an Image using Open AI</h3>
+      <input className='app-input' 
+      placeholder='Type something to Generate an Image..'
+      onChange={(e)=> setPrompt(e.target.value)}/>
+      <button onClick={generateImage}>Generate an Image</button>
+
+      {result.length > 0 ? <img className='result-img' src={result} alt='ai-image'/>: <></>}
     </div>
   )
 }
